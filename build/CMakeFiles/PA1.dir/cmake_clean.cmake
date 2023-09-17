0d1797a0e33198e313237a34948117066ccc50dc
WillowZuvich <143970627+WillowZuvich@users.noreply.github.com>
2023-09-17 16:00:46 -0700

    Add files via upload

---
+++ build/CMakeFiles/PA1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA1.dir/Game.cpp.o"
+  "CMakeFiles/PA1.dir/Game.cpp.o.d"
+  "CMakeFiles/PA1.dir/main.cpp.o"
+  "CMakeFiles/PA1.dir/main.cpp.o.d"
+  "CMakeFiles/PA1.dir/profile.cpp.o"
+  "CMakeFiles/PA1.dir/profile.cpp.o.d"
+  "PA1"
+  "PA1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
